147f3b2a5b9197a1dea534b31c6311f20540f08d
SungeunPark <elenability@naver.com>
2021-03-29 02:42:05 +0900

    Login.js/scss, Main.js/scss files uploaded

---
+++ src/pages/sungeunpark/Login/Login.js
@@ -0,0 +1,41 @@
+import React, { Component } from "react";
+//import { Link } from "react-router-dom";
+import { withRouter } from "react-router-dom";
+import "../pages/common.css";
+import "./Login.scss";
+
+// withRouterHOC
+
+class Login extends Component {
+  goToMain = () => {
+    this.props.history.push("/main");
+  };
+
+  render() {
+    return (
+      <div className="login">
+        <section className="header">Westagram</section>
+        <section className="login_area">
+          <input
+            type="text"
+            className="login_area_id"
+            placeholder="전화번호,사용자 이름 또는 이메일"
+          />
+          <input
+            type="password"
+            className="login_area_pw"
+            placeholder="비밀번호"
+          />
+          <button className="login_area_btn" onClick={this.goToMain}>
+            로그인
+          </button>
+        </section>
+        <section className="find_pw">
+          <a href="#">비밀번호를 잊으셨나요?</a>
+        </section>
+      </div>
+    );
+  }
+}
+
+export default withRouter(Login);
