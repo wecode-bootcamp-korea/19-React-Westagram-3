1d8bfb80f278a71da6f708c98003d023a8e01207
민석 / MIN / ミンソク <55354735+minsoku@users.noreply.github.com>
2021-04-01 17:03:23 +0900

    왜 머지 버튼이 안보이니

--- src/Test.js
+++ src/Test.js
@@ -5,6 +5,7 @@ class Test extends React.Component{
   render() {
     return(
       <>
+      <h1>안녕 3팀 !! </h1>
       <Link to="/loginjun">준형님</Link>
       <Link to="/loginhyo">효진님</Link>
       <Link to="/loginseok">서민석</Link>
